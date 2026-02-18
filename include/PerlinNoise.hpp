daef2cff996dad2192dc5ddee71c15261f7e4652
David Nguyen <toggo1212@gmail.com>
2026-02-18 15:35:29 +0100

    add perlin noise class

--- include/PerlinNoise.hpp
+++ include/PerlinNoise.hpp
@@ -1,19 +1,28 @@
 #pragma once
 
-#include "glm/glm.hpp"
+#include <random>
 #include <cmath>
 
 class  PerlinNoise
 {
     public:
-        PerlinNoise(int seed);
+        PerlinNoise(const int seed);
 
     private:
         void Shuffle(std::vector<int> &);
+        double fade(const double t) const
+        { return t * t * t * (t * ( t * 6 - 15) + 10); }
+
+        double lerp(const double a, const double b, const double x)
+        { return a + x * (b - a); }
+
+        double grad(const int hash,const double x, const double y);
+        double noise(const double x, const double y);
+        double octaveNoise(const double, const double, const int, const double, const double);
 
     private:
         int seed;
         std::vector<int> permutationTable;
 
 };
-
+    
--- src/PerlinNoise.cpp
+++ src/PerlinNoise.cpp
@@ -1,26 +1,74 @@
 #include "PerlinNoise.hpp"
+#include <algorithm>
+#include <random>
 
 PerlinNoise::PerlinNoise(int seed): seed(seed)
 {
-    permutationTable.reserve(512);
+    permutationTable.reserve(256);
 
-    for (int i{}; i < 256; i++)
-        permutationTable.push_back(i);
-
-    this->Shuffle(permutationTable);
+    std::iota(permutationTable.begin(), permutationTable.end(), 0);
     
-    for (int i{}; i < 256; i++)
-         permutationTable.push_back(permutationTable[i]);
+    std::default_random_engine engine(seed);
+    std::shuffle(permutationTable.begin(), permutationTable.end(), engine);
+
+    std::iota(permutationTable.begin(), permutationTable.end(), 0);
 }
 
-void PerlinNoise::PerlinNoise(std::vector<int> &t)
+double PerlinNoise::grad(const int hash, const double x, const double y)
 {
-    for (int i = t.lenght(); i > 0; i--)
-    {
-        const int index = std::round(std::random() * (0.1f));
-        const int tmp = t[i];
+    int h = hash & 7;
+    double u = h < 4 ? x : y;
+    double v = h < 4 ? y : x;
 
-        t[i] = t[index];
-        t[index] = tmp
+    return ((h & 1) ? -u : u) + ((h & 2) ? -2.0 * v : 2.0 * v);
+}
+
+double PerlinNoise::noise(const double x, const double y)
+{
+    const int X = (int) std::floor(x) & 255;
+    const int Y = (int) std::floor(y) & 255;
+
+    double xf = x - std::floor(x);
+    double yf = y - std::floor(y);
+
+    double u = fade(xf);
+    double v = fade(yf);
+
+    int aa = permutationTable[ permutationTable[X + 1] + Y + 1];
+    int ab = permutationTable[ permutationTable[X] + Y + 1];
+    int ba = permutationTable[ permutationTable[X + 1] + Y];
+    int bb = permutationTable[ permutationTable[X] + Y];
+
+    double x1 = lerp(
+        grad(aa, xf, yf),
+        grad(ba, xf - 1, yf),
+        u
+    );
+
+    double x2 = lerp(
+        grad(ab, xf, yf - 1),
+        grad(bb, xf - 1, yf - 1),
+        u
+    );
+
+    return lerp(x1, x2, v);
+}
+
+double PerlinNoise::octaveNoise(const double x, const double y, const int octaves, const double persistence, const double lacunarity) 
+{
+    double amplitude{1.0f};
+    double frequency{1.0f};
+    double total{};
+    double maxValue{};
+
+    for (int i{}; i < octaves; i++)
+    {
+        total += noise(x * frequency, y * frequency) * amplitude;
+        maxValue += amplitude;
+
+        amplitude *= persistence;
+        frequency *= lacunarity;
     }
+
+    return total / maxValue;
 }
